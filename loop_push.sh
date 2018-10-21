e70a63b1e903d8a81fb51284b9d420798e20275d
= <jason.ellis@yahoo.com>
2018-10-21 17:32:34 -0500

    update

--- loop_push.sh
+++ loop_push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # This is a bad way to do this, but for now... POC baby!
-while true ; do ./push.sh & sleep 60; done
+while true ; do /home/pi/Test/push.sh & sleep 60; done
 
--- loop_still.sh
+++ loop_still.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # This is a bad way to do this, but for now... POC baby!
-while true ; do ./still.sh & sleep 10; done
+while true ; do /home/pi/Test/still.sh & sleep 10; done
 
