5643cfda771cd569ddb7e80dee824b12ea8eab38
= <jason.ellis@yahoo.com>
2018-10-21 16:35:20 -0500

    unlink

--- capture.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Set our date
-DATE=$(date +"%Y-%m-%d_%H:%M:%S")
-
-# Capture an image at 1920x1200 at a very low quality (5 - or the file size is just stupidly big)
-raspistill -t 1 -n -q 5 -w 1920 -h 1200 -o /home/pi/Test/images/$DATE.jpg
-
-
